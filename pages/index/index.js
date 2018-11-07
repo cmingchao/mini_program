be9a585ef82e98c66b585c599b28dcd1a2e09dc5
chenmingchao <2439172011@qq.com>
2018-11-07 19:38:46 +0800

    第一次提交

---
+++ pages/index/index.js
@@ -0,0 +1,123 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    shopName:'',
+    list:[],
+    navList: ['推荐', '餐饮', '儿童亲子', '休闲愉快', '生活朋友', '服装朋友', '酒肉朋友', '其他'], //导航列表
+    winHeight: "",//窗口高度
+    currentTab: 0, //预设当前项的值
+    scrollLeft: 0, //tab标题的滚动条位置
+    expertList: [{ //假数据
+      img: "avatar.png",
+      name: "欢顔",
+      tag: "知名情感博主",
+      answer: 134,
+      listen: 2234
+    }]
+  },
+  inputShopName(e){
+    this.setData({
+      shopName: e.detail.value
+    });
+  },
+  // 滚动切换标签样式
+  switchTab: function (e) {
+    this.setData({
+      currentTab: e.detail.current
+    });
+    this.checkCor();
+  },
+  // 点击标题切换当前页时改变样式
+  swichNav: function (e) {
+    let cur = e.target.dataset.current;
+    if (this.data.currentTaB == cur) return false;
+    else {
+      this.setData({
+        currentTab: cur
+      });
+    }
+  },
+  //判断当前滚动超过一屏时，设置tab标题滚动条。
+  checkCor: function () {
+    let length = this.data.navList.length;
+    if (this.data.currentTab > 3) {
+      this.setData({
+        scrollLeft: (length-3)*100
+      });
+    } else {
+      this.setData({
+        scrollLeft: 0
+      });
+    }
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let that = this;
+    //  高度自适应
+    wx.getSystemInfo({
+      success: function (res) {
+        let clientHeight = res.windowHeight,
+          clientWidth = res.windowWidth,
+          rpxR = 750 / clientWidth;
+        let calc = clientHeight * rpxR - 150;
+        that.setData({
+          winHeight: calc
+        });
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/login/login.js
@@ -0,0 +1,177 @@
+import {
+  $http
+} from '../../utils/util.js';
+let reg = /^[1][3,4,5,6,7,8][0-9]{9}$/;
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    formData:{
+      phoneNumber:'',
+      validateCode:''
+    },
+    disabled:true,
+    loading:false,
+    disabledCode:true,
+    flag:true,
+    seconds:10,
+  },
+  //输入手机号
+  inputPhoneNumber(e){
+    let phoneNumber=e.detail.value.trim();
+    this.setData({
+      'formData.phoneNumber': phoneNumber
+    });
+    if (!phoneNumber){
+      this.setData({
+        disabledCode: true,
+        'formData.validateCode':''
+      });
+    }
+    this.isLogin();
+  },
+  // 输入验证码
+  inputValidateCode(e) {
+    let validateCode = e.detail.value.trim();
+    this.setData({
+      'formData.validateCode': validateCode
+    });
+    this.isLogin();
+  },
+  // 验证是否可登录
+  isLogin(){
+    let phoneNumber = this.data.formData.phoneNumber;
+    let validateCode = this.data.formData.validateCode;
+    if (phoneNumber && validateCode) {
+      this.setData({
+        disabled: false
+      });
+    } else {
+      this.setData({
+        disabled: true
+      });
+    }
+  },
+  //获取验证码
+  getValidateCode(){
+    let that=this;
+    let formData = that.data.formData;
+    that.setData({
+      seconds:10,
+    })
+    let seconds=that.data.seconds;
+    if (!formData.phoneNumber){
+      wx.showToast({
+        title: '请输入手机号',
+        icon: 'none'
+      });
+      return;
+    }
+    if (!reg.test(formData.phoneNumber)){
+      wx.showToast({
+        title: '请输入正确的手机号',
+        icon: 'none'
+      });
+      return;
+    }
+    that.setData({
+      flag:false,
+      disabledCode:false
+    });
+
+    let timer = setInterval(function(){
+      seconds--;
+      that.setData({
+        seconds
+      });
+      if (seconds<=0){
+        clearInterval(timer);
+        that.setData({
+          flag: true,
+          seconds:10
+        });
+      }
+    },1000);
+    // 发送请求获取验证码
+    // $http(formData).then(data=>{
+
+    // });
+  },
+  //登录
+  login(e){
+    let that=this;
+    let phoneNumber = that.data.formData.phoneNumber;
+    let validateCode = that.data.formData.validateCode;
+    if (!reg.test(phoneNumber)){
+      wx.showToast({
+        title: '请输入正确的手机号',
+        icon:'none'
+      });
+      return;
+    }
+    that.setData({
+      loading:true
+    });
+     // 发送请求登录
+    // $http(formData).then(data=>{
+
+    // });
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
